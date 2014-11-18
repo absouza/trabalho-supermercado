cf695988a3798575deb3cf3989d59a6d688152c8
absouza <absouza.93@gmail.com>
2014-11-17 23:47:39 -0300

    #1 - Inicio do trabalho

---
+++ trabalho-supermecado/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Alana
+ *
+ * Created on 17 de Novembro de 2014, 23:42
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
